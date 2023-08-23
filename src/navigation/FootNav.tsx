2d3ee4314d93bcd837ba08c696f1a02807913d39
Qkdrmf <djs110615@mju.ac.kr>
2023-08-23 17:43:23 +0900

    Feat: 네비게이션 백그라운드 적용

--- src/navigation/FootNav.tsx
+++ src/navigation/FootNav.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./FootNav.css";
 import { useState } from "react";
 import { BiSolidHomeCircle, BiHomeCircle, BiSearch, BiSolidSearch } from "react-icons/bi"; //BiSolidHomeCircle, BiHomeCircle
@@ -6,68 +6,32 @@ import { RiAddCircleLine, RiAddCircleFill } from "react-icons/ri";
 import {BsChat, BsChatFill, BsFillPersonFill, BsPerson } from "react-icons/bs";
 
 function FootNav() {
-  const [home, setHome] = useState(true);
-  const [search, setSearch] = useState(false);
-  const [add, setAdd] = useState(false);
-  const [chatting, setChatting] = useState(false);
-  const [more, setMore] = useState(false);
+  const [activeNav, setActiveNav] = useState(1);
 
-  const homeClick = () => {
-    setHome((current) => !current);
-    setSearch(false);
-    setAdd(false);
-    setChatting(false);
-    setMore(false);
-  };
+  const handleActiveNav = (nav: number) => {
+    setActiveNav(nav);
+  }
 
-  const searchClick = () => {
-    setSearch((current) => !current);
-    setHome(false);
-    setAdd(false);
-    setChatting(false);
-    setMore(false);
-  };
-
-  const addClick = () => {
-    setAdd((current) => !current);
-    setSearch(false);
-    setHome(false);
-    setChatting(false);
-    setMore(false);
-  };
-
-  const chattingClick = () => {
-    setChatting((current) => !current);
-    setSearch(false);
-    setAdd(false);
-    setHome(false);
-    setMore(false);
-  };
-
-  const moreClick = () => {
-    setMore((current) => !current);
-    setSearch(false);
-    setAdd(false);
-    setChatting(false);
-    setHome(false);
-  };
+  useEffect(() => {
+    console.log(activeNav);
+  }, [activeNav]);
 
   return (
     <nav className="wrapper">
       <div>
-        {home === true ? <BiSolidHomeCircle className="home" size="25" onClick={homeClick}/> : <BiHomeCircle className="home" size="25" onClick={homeClick}/>}
+        {activeNav === 1 ? <BiSolidHomeCircle size="25" /> : <BiHomeCircle size="25" onClick={() => handleActiveNav(1)}/>}
       </div>
       <div>
-        {search === true ? <BiSolidSearch className="search" size="25" onClick={searchClick}/> : <BiSearch className="search" size="25" onClick={searchClick}/>}
+        {activeNav === 2 ? <BiSolidSearch size="25" /> : <BiSearch size="25" onClick={() => handleActiveNav(2)}/>}
       </div>
       <div>
-        {add === true ? <RiAddCircleFill className="add" size="25" onClick={addClick}/> : <RiAddCircleLine className="add" size="25" onClick={addClick}/>}
+        {activeNav === 3 ? <RiAddCircleFill size="25" /> : <RiAddCircleLine size="25" onClick={() => handleActiveNav(3)}/>}
       </div>
       <div>
-        {chatting === true ? <BsChatFill className="chatting" size="25" onClick={chattingClick}/> : <BsChat className="chatting" size="25" onClick={chattingClick}/>}
+        {activeNav === 4 ? <BsChatFill size="25" /> : <BsChat size="25" onClick={() => handleActiveNav(4)}/>}
       </div>
       <div>
-        {more === true ? <BsFillPersonFill className="more" size="25" onClick={moreClick}/> : <BsPerson className="more" size="25" onClick={moreClick}/> }
+        {activeNav === 5 ? <BsFillPersonFill size="25" /> : <BsPerson size="25" onClick={() => handleActiveNav(5)}/> }
       </div>
     </nav>    
   );
